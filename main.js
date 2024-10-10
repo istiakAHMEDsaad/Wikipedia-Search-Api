0b50e6530b305b73533087036a91f5fd96ac42ff
istiakAHMEDsaad <istik2020@gmail.com>
2024-10-10 16:32:00 +0600

    add search button

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
-const themeToggleInput = document.getElementsByClassName('theme-controller')[0];
-themeToggleInput.addEventListener('click', function () {
-  // console.log('working');
-  const currentTheme = document.documentElement.getAttribute('data-theme');
-  const newTheme = currentTheme === 'light' ? 'business' : 'light';
-  document.documentElement.setAttribute('data-theme', newTheme);
-});
-
 document.addEventListener('DOMContentLoaded', () => {
-  const myForm = document.getElementById('my-form');
-  const inputValue = document.querySelector('input[type="search"]');
+  // const myForm = document.getElementById('my-form');
+  const searchBtn = document.getElementById('search');
+  const input = document.querySelector('input[type="search"]');
   const resultCount = document.getElementById('result-count');
   const resultContainer = document.getElementById('final-results');
 
-  myForm.addEventListener('submit', (event) => {
+  searchBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    const searchValue = inputValue.value;
-    if (searchValue) {
-      searchWikipedia(searchValue);
+    const inputValue = input.value;
+    if (inputValue) {
+      searchWikipedia(inputValue);
     }
   });
 
-  const searchWikipedia = (searchValue) => {
+  // ------------- Fetch Data -------------
+  const searchWikipedia = (inputValue) => {
     const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=500&srsearch=${encodeURIComponent(
-      searchValue
+      inputValue
     )}`;
-
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
@@ -33,18 +26,29 @@ document.addEventListener('DOMContentLoaded', () => {
       .catch((error) => alert('Error:', error));
   };
 
+  // ------------- Show Search Data -------------
   const showResults = (results) => {
     resultContainer.innerHTML = '';
     resultCount.textContent = `Resuls Count: ${results.length}`;
 
+    // loop
     results.forEach((result) => {
       const resultElement = document.createElement('div');
-      resultElement.classList.add('border', 'px-4', 'py-2', 'flex', 'flex-col', 'items-center', 'space-y-2');
+      resultElement.classList.add(
+        'border',
+        'rounded-md',
+        'px-4',
+        'py-2',
+        'flex',
+        'flex-col',
+        'items-center',
+        'space-y-2'
+      );
       resultElement.innerHTML = `
-        <h3 class="text-2xl text-start font-bold">${result.title}</h3>
-        <p>${result.snippet}</p>
-        <a class="w-full text-center font-semibold text-base text-white py-2 rounded-lg cursor-pointer bg-slate-500 hover:bg-slate-400 transition-all" href="https://en.wikipedia.org/?curid=${result.pageid}" target="_blank">Read More</a>
-      `;
+    <h3 class="text-2xl text-start font-bold">${result.title}</h3>
+    <p>${result.snippet}</p>
+    <a class="w-full text-center font-semibold text-base text-white py-2 rounded-lg cursor-pointer bg-slate-500 hover:bg-slate-400 transition-all" href="https://en.wikipedia.org/?curid=${result.pageid}" target="_blank">Read More</a>
+  `;
       resultContainer.appendChild(resultElement);
     });
   };
