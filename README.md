27b65d101dbd953b515b16b979f8bc8d115a026c
istiakAHMEDsaad <istik2020@gmail.com>
2024-10-10 14:53:47 +0600

    complete project

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
 
+
 # Wikipedia Search By Api (for educational purpose)
 
 You can search everything and your desired data will fetched by wiki api and show to you.
+
+## Screen Shot
+![Mobile Device Screenshot](./assets/wiki.PNG)
+
+## Live link:
+[Wiki Clone](https://istiakahmedsaad.github.io/Wikipedia-Search-Api/)
+
+
+## Installation
+
+Install this project with npm
+
+install latest [Node.js](https://nodejs.org/en/download/package-manager)
+
+```bash
+  1. npm install -D tailwindcss
+  2. npm i -D daisyui@latest
+  3. npm run dev
+  4. default port :5500
+```
+    
+## API Reference
+
+#### Get all items
+
+```http
+  https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=500&srsearch=${encodeURIComponent(
+      searchValue
+    )}
+```
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `searchValue` | `string` | pass this Parameter to your function |
+
+#### Get item
+
+```http
+  https://en.wikipedia.org/?curid=${result.pageid}
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `result`      | `string` | result value from your search data |
+ 
+
+## Idea from
+
+- [@AsmrProg-YT](https://github.com/AsmrProg-YT)
--- main.js
+++ main.js
@@ -5,3 +5,47 @@ themeToggleInput.addEventListener('click', function () {
   const newTheme = currentTheme === 'light' ? 'business' : 'light';
   document.documentElement.setAttribute('data-theme', newTheme);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const myForm = document.getElementById('my-form');
+  const inputValue = document.querySelector('input[type="search"]');
+  const resultCount = document.getElementById('result-count');
+  const resultContainer = document.getElementById('final-results');
+
+  myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const searchValue = inputValue.value;
+    if (searchValue) {
+      searchWikipedia(searchValue);
+    }
+  });
+
+  const searchWikipedia = (searchValue) => {
+    const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=500&srsearch=${encodeURIComponent(
+      searchValue
+    )}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        showResults(data.query.search);
+      })
+      .catch((error) => alert('Error:', error));
+  };
+
+  const showResults = (results) => {
+    resultContainer.innerHTML = '';
+    resultCount.textContent = `Resuls Count: ${results.length}`;
+
+    results.forEach((result) => {
+      const resultElement = document.createElement('div');
+      resultElement.classList.add('border', 'px-4', 'py-2', 'flex', 'flex-col', 'items-center', 'space-y-2');
+      resultElement.innerHTML = `
+        <h3 class="text-2xl text-start font-bold">${result.title}</h3>
+        <p>${result.snippet}</p>
+        <a class="w-full text-center font-semibold text-base text-white py-2 rounded-lg cursor-pointer bg-slate-500 hover:bg-slate-400 transition-all" href="https://en.wikipedia.org/?curid=${result.pageid}" target="_blank">Read More</a>
+      `;
+      resultContainer.appendChild(resultElement);
+    });
+  };
+});
